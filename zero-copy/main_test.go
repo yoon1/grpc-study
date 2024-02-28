455fc3d8db148ba7b5590717f1294017ddb077b7
yoon1 <o_ink@naver.com>
2024-02-29 03:18:03 +0900

    chore: remove comments

--- zero-copy/main_test.go
+++ zero-copy/main_test.go
@@ -6,16 +6,12 @@ const sourceFile = "source.txt"
 const destinationWithoutZeroCopy = "destination_without_zero_copy.txt"
 const destinationWithZeroCopy = "destination_with_zero_copy.txt"
 
-// BenchmarkCopyWithoutZeroCopy
-// BenchmarkCopyWithoutZeroCopy-10    	   21362	     59832 ns/op
 func BenchmarkCopyWithoutZeroCopy(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		copyWithoutZeroCopy(sourceFile, destinationWithoutZeroCopy)
 	}
 }
-BenchmarkCopyWithZeroCopy
-BenchmarkCopyWithZeroCopy-10    	   22272	     53925 ns/op
 
 func BenchmarkCopyWithZeroCopy(b *testing.B) {
 	b.ResetTimer()
